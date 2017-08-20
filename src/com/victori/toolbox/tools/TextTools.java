ec5c6c4b2ca89c729a93ac2788985c9861d8215f
Pedro Victori <fosforilasa@gmail.com>
2017-08-20 20:24:11 +0100

    TextTools now needs path to files parameter

--- src/com/victori/toolbox/tools/TextTools.java
+++ src/com/victori/toolbox/tools/TextTools.java
@@ -11,9 +11,9 @@ import com.victori.toolbox.utils.TextFile;
  */
 
 public class TextTools {
-	public static String genRandomName(String sylFile,String prefixFile,int nSyl,boolean prefix){ //generates a random name of nSyl syllables taken from filename, with randomly occurring prefix when prefix=TRUE
+	public static String genRandomName(String sylFile,String prefixFile,int nSyl,boolean prefix, String path){ //generates a random name of nSyl syllables taken from filename, with randomly occurring prefix when prefix=TRUE
 		String name=null;
-		TextFile syllables = new TextFile(sylFile);
+		TextFile syllables = new TextFile(sylFile, path);
 		int linesNumber = syllables.getLinesNumber(); //number of lines in this file
 		int[] randomNumbers = MathTools.genRandomUniqueIntegers(nSyl+2,1,linesNumber); //we generate an extra random number to decide if we add a prefix, and another one to decide which prefix
 		for (int i=0;i<nSyl;i++){
@@ -24,7 +24,7 @@ public class TextTools {
 
 		name = firstLetterToUpperCase(name);
 		if(prefix && randomNumbers[nSyl]%2==0){ //if the random number is even (p=0.5) we add a prefix) [nSyl] indicates the number before last, the last one would be nSyl+1, since the length of the array is nSyl+2
-			TextFile prefixes = new TextFile(prefixFile);
+			TextFile prefixes = new TextFile(prefixFile, path);
 			int pLinesNumber = prefixes.getLinesNumber(); //number of lines in this file
 			long dPrefixNumber = Math.round(((double)randomNumbers[nSyl+1]*(double)pLinesNumber)/(double)linesNumber);//we transform the random number (which is between 0 and linesNumber from syllables file) into a number that is between 0 and the numbers of lines of the prefix file
 			int prefixNumber = Math.toIntExact(dPrefixNumber);
